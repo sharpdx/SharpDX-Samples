d74ed756672d48763f850d25eefcf45fb5b9305e
Alexandre Mutel <alexandre_mutel@live.com>
2014-05-09 07:49:14 +0900

    [Build] Add RestoreNugetPackages.bat

---
+++ RestoreNugetPackages.bat
@@ -0,0 +1,5 @@
+@echo off
+.nuget\nuget.exe restore WindowsDesktop.sln
+.nuget\nuget.exe restore Toolkit.sln
+.nuget\nuget.exe restore WindowsAppStore81.sln
+.nuget\nuget.exe restore WindowsPhone8.sln
