9d2662230cf667f20ac3543622db70d71b6ae2e1
RobyDX <robydx@gmail.com>
2015-08-19 11:04:03 +0200

    [Direct3D12] Added D3D12 Hello World Samples

---
+++ Desktop/Direct3D12/HelloConstBuffers/Program.cs
@@ -0,0 +1,38 @@
+﻿using SharpDX.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloConstBuffers
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var form = new RenderForm("Hello Constant Buffer");
+            form.Width = 1280;
+            form.Height = 800;
+            form.Show();
+
+            using (HelloConstBuffers app = new HelloConstBuffers())
+            {
+                app.Initialize(form);
+
+                using (var loop = new RenderLoop(form))
+                {
+                    while (loop.NextFrame())
+                    {
+                        app.Update();
+                        app.Render();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Desktop/Direct3D12/HelloTexture/Program.cs
@@ -0,0 +1,38 @@
+﻿using SharpDX.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloTexture
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var form = new RenderForm("Hello Texture");
+            form.Width = 1280;
+            form.Height = 800;
+            form.Show();
+            
+            using (HelloTexture app = new HelloTexture())
+            {
+                app.Initialize(form);
+
+                using (var loop = new RenderLoop(form))
+                {
+                    while (loop.NextFrame())
+                    {
+                        app.Update();
+                        app.Render();
+                    }
+                }
+            }
+        }
+    }
+}
