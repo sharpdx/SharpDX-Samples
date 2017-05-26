f0800fe11252102b0fa16aabbd85d857d0ac81c8
Aaron Sun <axiverse@gmail.com>
2017-05-26 00:49:28 -0700

    Properly dispose of font loader

--- Desktop/DirectWrite/CustomFont/CustomFont.cs
+++ Desktop/DirectWrite/CustomFont/CustomFont.cs
@@ -218,5 +218,19 @@ namespace CustomFont
 
         }
 
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+
+                CurrentResourceFontLoader.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
